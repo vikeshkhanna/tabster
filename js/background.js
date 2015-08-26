ebaf662a6f86f290978f77712d98a5b2f97791e3
Vikesh Khanna <khanna.vikesh@gmail.com>
2015-08-26 03:46:59 -0700

    Adding files

---
+++ js/background.js
@@ -0,0 +1,127 @@
+console.log("<tabster> Background page loaded");
+
+// Maps the tab ID to the latest revision number
+var tabMap = {}
+// The core index. Maps each word to a list of 
+var index = {}
+
+// Prune invalid pages in index on every 100th request
+var pruneInterval = 10;
+var indexUpdateRequestCnt = 0;
+
+function updateIndex(tab, keyWords) {
+  var tabId = tab.id;
+
+  // Update revision
+  if (!(tabId in tabMap)) {
+    tabMap[tabId] = 0;
+  }
+
+  tabMap[tabId] += 1;
+  indexUpdateRequestCnt += 1;
+
+  for (word in keyWords) {
+    // Discard empty string and such
+    if (word) {
+      var cnt = keyWords[word];
+      
+      // Initialize with empty list
+      if (!(word in index)) {
+        index[word] = [];
+      }
+
+      console.log(word);
+
+      // Push the tab object
+      index[word].push({
+        tab : tab,
+        revision : tabMap[tabId],
+        rank : cnt
+      });
+    }
+  }
+
+  // console.log(index);
+  if (indexUpdateRequestCnt % pruneInterval == 0) {
+    pruneIndex();
+  }
+}
+
+function pruneIndex() {
+  console.log("<tabster> Pruning index");
+  for (word in index) {
+    var tabs = index[word];
+    var toDeleteIndices = [];
+
+    tabs.forEach(function(tabItem, i, arr) {
+      var latestRevision = tabMap[tabItem.tab.id];
+
+      if (obj.revision != latestRevision) {
+        toDeleteIndices.push(i);
+      }
+    });
+
+    toDeleteIndices.forEach(function(index, i, arr) {
+      delete tabs[index];
+    });
+  }
+}
+
+function search(text) {
+  // Rank Ordered list of tab IDs
+  var result = [];
+  var ranks = {};
+  var words = text.split(/[ ,]+/);
+
+  // For each word in query
+  words.forEach(function(elem, i, arr) {
+    var word = stemmer(elem.toLowerCase());
+    if (word in index) {
+      console.log("<tabster> " + word + " found in index!");
+      var tabs = index[word];
+
+      tabs.forEach(function(tabItem, i, arr) {
+        // tab is latest
+        if (tabItem.revision == tabMap[tabItem.tab.id]) {
+          if (!(tabItem.tab.id in ranks)) {
+            ranks[tabItem.tab.id] = {
+              rank: 0,
+              tab: tabItem.tab
+          };
+        }
+
+        // Add rank contributed by this tab
+        ranks[tabItem.tab.id].rank += tabItem.rank;
+      }
+    });
+  }
+});
+
+var tabs = Object.keys(ranks).map(function(key) {
+  return ranks[key].tab;
+})
+// Sort in the descending order of ranks.
+return tabs.sort(function(tab1, tab2) { return ranks[tab2.id] - ranks[tab1.id] });
+}
+
+chrome.runtime.onMessage.addListener(
+function(request, sender, sendResponse) {
+  var args = request.args;
+  console.log("<tabster> method : " + request.method);
+  console.log("<tabster> args : " + args);
+
+  if (request.method === "updateIndex") {
+      updateIndex(sender.tab, args);
+      sendResponse("Updated index for Id : " + sender.tab.id);
+  } else if (request.method === "search") {
+      sendResponse(search(args));
+  } else if (request.method === "switchToTab") {
+      // chrome.tabs is unavailable in content scripts
+      chrome.tabs.update(parseInt(args), { selected: true });
+      sendResponse("Switched to tab: " + args);
+  } else {
+      console.log("<tabster> invalid request");
+      sendResponse("Error");
+    }
+  }
+);
---
+++ js/content_script.js
@@ -0,0 +1,108 @@
+var stopWords  = ["the", "a", "an", "is", "he", "she"];
+
+var SEARCH_ON = false;
+var currentTabs = null;
+
+function strip(text) {
+  var htmlRemoved = text.replace(/<(?:.|\n)*?>/gm, '');
+  var clean = htmlRemoved.replace(/[^A-Za-z0-9 ]/g, '');
+  return clean;
+}
+
+function getKeywords(text) {
+  var words = text.split(/[ ,]+/);
+  var keywords = {};
+
+  words.forEach(function(elem, i, arr) {
+    var word = stemmer(elem.toLowerCase());
+    if (!stopWords.indexOf(word) >= 0) {
+      if (!(word in keywords)) {
+        keywords[word] = 0;
+      }
+      keywords[word] += 1;
+    }
+  });
+
+  return keywords;
+}
+
+
+var strippedText = strip(document.body.textContent);
+var keyWords = getKeywords(strippedText);
+
+console.log(keyWords);
+
+// Send message to the background page
+chrome.runtime.sendMessage({ method: "updateIndex", args: keyWords}, function(response) {
+    console.log(response);
+});
+
+function onKeyUp(evt) {
+  // Ctrl+Enter
+  if (evt.ctrlKey && evt.keyCode == 13) {
+    searchOverlay.style.display = "block";
+    searchBar.focus();
+    SEARCH_ON = true;
+  } else if (evt.keyCode == 27) {
+    searchOverlay.style.display = "none";
+    SEARCH_ON = false;
+  }
+}
+
+var searchOverlay = document.createElement("div");
+searchOverlay.id = "tabster-search-overlay";
+searchOverlay.innerHTML = "<input type='text' id='tabster-search-bar' placeholder='Search...' /><span id='tabster-tip'>Tip: Go to tab with Ctrl + [tab #]</span>" + 
+  "<hr /><ul id='tabster-search-items'></ul>";
+
+document.body.appendChild(searchOverlay);
+
+// Add keypress listened
+if (window == top) {
+  window.addEventListener('keyup', onKeyUp, false);
+}
+
+var searchBar = document.getElementById("tabster-search-bar");
+var searchItems = document.getElementById("tabster-search-items");
+
+searchBar.onkeyup = function(evt) {
+
+  if (SEARCH_ON && evt.ctrlKey && ((evt.keyCode >= 96 && evt.keyCode <= 105) || (evt.keyCode >=48 && evt.keyCode <= 57))) {
+    var id = (evt.keyCode >= 96 && evt.keyCode <= 105 ? evt.keyCode - 96 : evt.keyCode - 48);
+    chrome.runtime.sendMessage({ method: "switchToTab", args: currentTabs[id].id });
+    return;
+  }
+
+  var text = evt.target.value;
+  // Send message to the background page
+  chrome.runtime.sendMessage({ method: "search", args: text}, function(tabs) {
+    if (text && searchBar.value == text) {
+      // Clear list
+      searchItems.innerHTML = '';
+      console.log("<tabster> Search results: ")
+      console.log(tabs);
+
+      // Set the global currentTabs to this result.
+      currentTabs = tabs;
+
+      tabs.forEach(function(tab, i, arr) {
+        var li = document.createElement("li");
+        li.innerHTML = "";
+
+        if (tab.favIconUrl) {
+          li.innerHTML = "<img src='"  + tab.favIconUrl + "' />";
+        }
+
+        li.innerHTML += "<div class='tabster-search-item-content'><h3>[" + i + "] " + tab.title + 
+          "</h3><span>" + tab.url + "</span></div>";
+
+        li.onclick = function() {
+          chrome.runtime.sendMessage({ method: "switchToTab", args: currentTabs[id].id });
+        }
+
+        searchItems.appendChild(li);
+      });
+    } else {
+      console.log("<tabster> Rejecting old search request");
+    }
+  });
+};
